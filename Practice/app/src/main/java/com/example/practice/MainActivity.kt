6d22f28fb816aa60992bd32e32cc6bbb4926a1ef
envzhu <envzhu@gmail.com>
2020-12-31 07:27:42 +0900

    Add Button code to practice

--- Practice/app/src/main/java/com/example/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/example/practice/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Log.e("Button", "Clicked")
+        }
     }
 }
